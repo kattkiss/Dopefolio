b16184dfd1a381ef4645f415c00cbb493ce28360
kattkiss <39883181+kattkiss@users.noreply.github.com>
2026-03-25 14:35:28 +0100

    Image scrollers added

--- index.js
+++ index.js
@@ -53,3 +53,33 @@ elements.forEach(el => {
   el.classList.add('fade-in');
   observer.observe(el);
 });
+
+const mainImg = document.querySelector('.gallery-carousel__main-img');
+const thumbs = document.querySelectorAll('.gallery-carousel__thumb');
+const prevBtn = document.querySelector('.gallery-carousel__nav--prev');
+const nextBtn = document.querySelector('.gallery-carousel__nav--next');
+
+let currentIndex = 0;
+
+function updateMainImage(index) {
+  thumbs.forEach(t => t.classList.remove('active'));
+  thumbs[index].classList.add('active');
+  mainImg.src = thumbs[index].src;
+  currentIndex = index;
+}
+
+thumbs.forEach((thumb, i) => {
+  thumb.addEventListener('click', () => updateMainImage(i));
+});
+
+prevBtn.addEventListener('click', () => {
+  let newIndex = currentIndex - 1;
+  if (newIndex < 0) newIndex = thumbs.length - 1;
+  updateMainImage(newIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  let newIndex = currentIndex + 1;
+  if (newIndex >= thumbs.length) newIndex = 0;
+  updateMainImage(newIndex);
+});
